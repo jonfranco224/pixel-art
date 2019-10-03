eea2a762b6d70f7e3092c612b59fd8376024ea5b
JonFranco <jonfranco224@gmail.com>
2019-10-03 11:32:43 -0400

    adding back in features

--- script.js
+++ script.js
@@ -128,41 +128,34 @@ function blend (r1, g1, b1, r2, g2, b2) {
 
 let $ = {
   FRAMES: [],
+  LAYERS: [],
   frameActive: 0,
   layerActive: 31,
   colorActive: 20,
-  toolActive: 'pencil'
+  toolActive: 'pencil',
+  layers: [{ name: 'Layer 1', hidden: false, locked: false }]
 }
-
-const FRAMES = []
 
 function setToolActive (e, tool) {
   document.querySelector('#tools').querySelector(`[data-tool="${$.toolActive}"]`).classList.remove('active')
   $.toolActive = e ? e.target.dataset.tool : tool
   document.querySelector('#tools').querySelector(`[data-tool="${$.toolActive}"]`).classList.add('active')
 }
-
-function setLayerActive (e, i) {
-  document.querySelector(`[data-layerindex="${$.layerActive}"]`).classList.remove('active')
-  $.layerActive = e ? parseInt(e.target.dataset.i) : i
-  document.querySelector(`[data-layerindex="${$.layerActive}"]`).classList.add('active')
-}
-
 function setColorActive (e, i) {
   document.querySelector(`[data-colorindex="${$.colorActive}"]`).classList.remove('active')
   $.colorActive = e ? parseInt(e.target.dataset.colorindex) : i
   document.querySelector(`[data-colorindex="${$.colorActive}"]`).classList.add('active')
 }
 
-function modalOpen (e) {
+function modalOpen (e, target) {
+  const val = e ? e.target.dataset.val : target
   document.querySelector(`#modal`).classList.add('visible')
-  document.querySelector(`#modal #${e.target.dataset.val}`).classList.add('visible')
+  document.querySelector(`#modal #${val}`).classList.add('visible')
 }
-
-function modalClose (e, str) {
-  const id = e ? e.target.dataset.val : str
+function modalClose (e, target) {
+  const val = e ? e.target.dataset.val : target
   document.querySelector(`#modal`).classList.remove('visible')
-  document.querySelector(`#modal #${id}`).classList.remove('visible')
+  document.querySelector(`#modal #${val}`).classList.remove('visible')
 }
 
 // INIT COLORS
@@ -177,7 +170,7 @@ COLORS[3] = 0
 COLORS[4] = 75 // Selected
 COLORS[5] = 75
 COLORS[6] = 75
-COLORS[7] = 125
+COLORS[7] = 175
 
 COLORS[8] = 128 // Transparency 1
 COLORS[9] = 128
@@ -225,7 +218,6 @@ COLORS[41] = 0
 COLORS[42] = 0
 COLORS[43] = 255
 
-
 CANVAS = {
   w: 0,
   h: 0,
@@ -240,12 +232,6 @@ CANVAS = {
 }
 
 const CANVAS_DOM = document.querySelector('#main-canvas')
-CANVAS_DOM.addEventListener('mousemove', canvasPaint)
-CANVAS_DOM.addEventListener('mousedown', canvasPaint)
-CANVAS_DOM.addEventListener('mouseup', canvasPaint)
-CANVAS_DOM.addEventListener('mousedown', (e) => { CANVAS.mouseDown = true })
-CANVAS_DOM.addEventListener('mouseup', (e) => { CANVAS.mouseDown = false })
-CANVAS_DOM.addEventListener('mouseleave', (e) => { CANVAS.mouseDown = false })
 
 // INIT CANVAS
 const canvas = document.querySelector('#main-canvas')
@@ -307,7 +293,7 @@ function canvasInit (w, h) {
   }
 
   // Center canvas
-  wrapper.parentNode.scrollLeft = ((CANVAS.w * CANVAS.scaleRatio) / 2) - (wrapper.parentNode.offsetWidth / 2)
+  wrapper.parentNode.scrollLeft = ((CANVAS.w * CANVAS.scaleRatio) / 2) - (wrapper.parentNode.offsetWidth / 2) + 20
   wrapper.parentNode.scrollTop = ((CANVAS.h * CANVAS.scaleRatio) / 2) - (wrapper.parentNode.offsetHeight / 2) + 20
 
   // Draw Transparency layer
@@ -335,7 +321,7 @@ function canvasInit (w, h) {
 }
 
 function getBlendedColorAtPt (x, y) {
-  const i = (~~(x)) + CANVAS.w * (~~(y))
+  const i = x + CANVAS.w * y
   return CURR_FRAME[(i * 36) + $.layerActive] + 4
 }
 function getColorAtPt (x, y) {
@@ -561,9 +547,26 @@ function selectionWrite () {
   CANVAS.isSelected = false
 }
 
-function canvasPaint (e) {
+CANVAS_DOM.addEventListener('mousemove', canvasPaint)
+CANVAS_DOM.addEventListener('mousedown', canvasPaint)
+CANVAS_DOM.addEventListener('mouseup', canvasPaint)
+CANVAS_DOM.addEventListener('mouseleave', canvasPaint)
 
-  CANVAS_DOM.style.cursor = 'default'
+function canvasPaint (e) {
+  if (e.type === 'mousedown' && e.target.id !== 'main-canvas') {
+    return
+  }
+
+  if (CANVAS.mouseDown && e.type === 'mouseleave') {
+    return
+  }
+
+  if ($.LAYERS[$.layerActive].locked || $.LAYERS[$.layerActive].hidden) {
+    CANVAS_DOM.style.cursor = 'not-allowed'
+    return
+  } else {
+    CANVAS_DOM.style.cursor = 'crosshair'
+  }
 
   CANVAS.prev.x = CANVAS.curr.x
   CANVAS.prev.y = CANVAS.curr.y
@@ -573,14 +576,19 @@ function canvasPaint (e) {
   CANVAS.end.x = CANVAS.curr.x
   CANVAS.end.y = CANVAS.curr.y
 
+  // Hover
+  // if (!CANVAS.mouseDown && $.toolActive !== 'move' && $.toolActive !== 'select') {
+  //   setPreviewPoint(CANVAS.prev.x, CANVAS.prev.y, 0)
+  //   setPreviewPoint(CANVAS.curr.x, CANVAS.curr.y, $.colorActive)
+  // }
+  // if (!CANVAS.mouseDown && e.type === 'mouseleave') {
+  //   setPreviewPoint(CANVAS.curr.x, CANVAS.curr.y, 0)
+  // }
+
   if (e.type === 'mousedown') {
     CANVAS.mouseDown = true
     CANVAS.start.x = CANVAS.curr.x
     CANVAS.start.y = CANVAS.curr.y
-  }
-
-  if (e.type === 'mouseup') {
-    CANVAS.mouseDown = false
   }
 
   if ($.toolActive === 'pencil' || $.toolActive === 'eraser') {
@@ -605,11 +613,6 @@ function canvasPaint (e) {
       if ($.toolActive === 'line') line('preview', CANVAS.start, CANVAS.end, $.colorActive)
       if ($.toolActive === 'square') square('preview', CANVAS.start, CANVAS.end, $.colorActive)
     }
-
-    if (e.type === 'mouseup') {
-      if ($.toolActive === 'line') line('print', CANVAS.start, CANVAS.end, $.colorActive)
-      if ($.toolActive === 'square') square('print', CANVAS.start, CANVAS.end, $.colorActive)
-    }
   }
 
   if ($.toolActive === 'select') {
@@ -620,12 +623,6 @@ function canvasPaint (e) {
 
     if (CANVAS.mouseDown) {
       squareSolid('selection', CANVAS.start, CANVAS.end)
-    }
-
-    if (e.type === 'mouseup') {
-      selectionLift(CANVAS.start, CANVAS.end)
-      CANVAS.customSelection = true
-      setToolActive(undefined, 'move')
     }
   }
 
@@ -642,10 +639,8 @@ function canvasPaint (e) {
       }
     }
 
-    if (CANVAS.mouseDown) selectionTranslate(xStep, yStep)
-
-    if (e.type === 'mouseup') {
-      selectionWrite()
+    if (CANVAS.mouseDown) {
+      selectionTranslate(xStep, yStep)
     }
   }
 }
@@ -678,18 +673,24 @@ function canvasGetTopColor (pixelIndex, offset) {
     else if ((v & 1) !== 0) index += 7
   }
 
-  if (index !== -1) {
-    return index
-  } else {
-    return 31
-  }
-
+  return index
+}
+function canvasSetBuf32 (pixel, color) {
+  buf32[pixel] =
+   (COLORS[color + 3] << 24) |    // alpha
+   (COLORS[color + 2] << 16) |    // blue
+   (COLORS[color + 1] <<  8) |    // green
+    COLORS[color];
 }
 function canvasDraw () {
   let toggle = 0
-  let previewColorIndex = 0
-  let colorIndex = 0
-  let topVisIndex = 0
+
+  let colorPrev = 0
+  let colorMain = 0
+  let colorVisible = 0
+
+  let pixel = 0
+  let idx = 0
 
   // Draw final frame buffer
   function draw () {
@@ -700,36 +701,38 @@ function canvasDraw () {
       return
     }
 
+    pixel = 0
+    pixel36 = 0
+
     //console.time('draw loop')
-    for (let pixI = 0, idx = 0; pixI < pixelResTotal; pixI++, idx+=36) {
-      topVisIndex = canvasGetTopColor(pixI, idx)
-      colorIndex = CURR_FRAME[idx + topVisIndex] // step 1: grab index of first visible layer
-      previewColorIndex = previewBuffer[pixI]
+    while (pixel < pixelResTotal) {
+      colorVisible = canvasGetTopColor(pixel, pixel36)
+      colorPrev = previewBuffer[pixel]
 
-      buf32[pixI] =
-       (COLORS[colorIndex + 3] << 24) |    // alpha
-       (COLORS[colorIndex + 2] << 16) |    // blue
-       (COLORS[colorIndex + 1] <<  8) |    // green
-        COLORS[colorIndex];
+      if (colorPrev === 0 && colorVisible === -1) {
+        canvasSetBuf32(pixel, 0)
+      } else if (colorPrev === 4 && colorVisible === -1) {
+        canvasSetBuf32(pixel, 4)
+      } else {
+        colorMain = CURR_FRAME[pixel36 + colorVisible] // step 1: grab index of first visible layer // if (colorMain === 1) console.log('possible bug here')
 
-      // Preview is being written directly to by translated selection
-      // so if there is a color at this preview index, write the preview color
-      if (previewColorIndex !== 0 && previewColorIndex !== 4 && ($.layerActive <= topVisIndex)) {
-        buf32[pixI] =
-         (COLORS[previewColorIndex + 3] << 24) |    // alpha
-         (COLORS[previewColorIndex + 2] << 16) |    // blue
-         (COLORS[previewColorIndex + 1] <<  8) |    // green
-          COLORS[previewColorIndex];
-      // if there is no color at preview index, write blended version of what the point is in the frame
-      } else if (previewColorIndex === 4 && ($.layerActive <= topVisIndex)) {
-        buf32[pixI] =
-          (COLORS[colorIndex + 4 + 3] << 24) |    // alpha
-          (COLORS[colorIndex + 4 + 2] << 16) |    // blue
-          (COLORS[colorIndex + 4 + 1] <<  8) |    // green
-           COLORS[colorIndex + 4];
+        canvasSetBuf32(pixel, colorMain)
+
+        // Preview is being written directly to by translated selection so
+        // if there is a color at this preview index, write the preview color
+        // else if there is no color at preview index, write blended version of what the point is in the frame
+        if (colorPrev !== 0 && colorPrev !== 4 && ($.layerActive <= colorVisible || colorVisible === -1)) {
+          canvasSetBuf32(pixel, colorPrev)
+        } else if (colorPrev !== 0 && colorPrev === 4 && ($.layerActive <= colorVisible || colorVisible === -1)) {
+          canvasSetBuf32(pixel, colorMain + 4)
+        }
       }
+
+      pixel += 1
+      pixel36 += 36
     }
     //console.timeEnd('draw loop')
+
     pixelBuffer.data.set(buf8);
     ctx.putImageData(pixelBuffer, 0, 0)
 
@@ -755,13 +758,305 @@ function canvasZoomOut () {
   CANVAS_CONTAINER.children[0].style.transform = `scale(${1 / CANVAS.zoom})`
   CANVAS_CONTAINER.children[1].style.transform = `scale(${1 / CANVAS.zoom})`
 
-  CANVAS_CONTAINER.parentNode.scrollLeft = ((CANVAS.w * CANVAS.scaleRatio) / 2) - (CANVAS_CONTAINER.parentNode.offsetWidth / 2)
+  CANVAS_CONTAINER.parentNode.scrollLeft = ((CANVAS.w * CANVAS.scaleRatio) / 2) - (CANVAS_CONTAINER.parentNode.offsetWidth / 2) + 20
   CANVAS_CONTAINER.parentNode.scrollTop = ((CANVAS.h * CANVAS.scaleRatio) / 2) - (CANVAS_CONTAINER.parentNode.offsetHeight / 2) + 20
+}
+
+function layersInit () {
+  // Insert DOM placeholders
+  const layerTemplate = document.querySelector("[data-layerindex='31']")
+  const layerWrapper = document.querySelector("#tl-layers")
+
+  for (let i = 30; i >= 0; i--) {
+    const clone = layerTemplate.cloneNode(true)
+    clone.setAttribute('data-layerindex', i)
+    clone.style.display = 'none'
+
+    for (let a = 0; a < clone.children.length; a++) {
+      clone.children[a].setAttribute('data-i', i)
+    }
+
+    layerWrapper.insertBefore(clone, layerWrapper.children[0])
+  }
+
+  // Fill out state array
+  for (let i = 0; i < 32; i++) {
+    if (i === 31) {
+      $.LAYERS[i] = { name: 'Layer 1', hidden: false, locked: false }
+    } else {
+      $.LAYERS[i] = { name: undefined, hidden: undefined, locked: undefined }
+    }
+  }
+
+  layersUpdate()
+}
+function layersSetActive (i) {
+  $.layerActive = i
+
+  layersUpdate()
+}
+function layersAdd () {
+  for (let i = $.LAYERS.length - 1; i >= 0 ; i--) {
+    if ($.LAYERS[i].name === undefined && $.LAYERS[i].hidden === undefined && $.LAYERS[i].locked === undefined) {
+      $.LAYERS[i].name = `Layer ${Math.abs(i - $.LAYERS.length)}`
+      $.LAYERS[i].hidden = false
+      $.LAYERS[i].locked = false
+
+      layersUpdate()
+
+      return
+    }
+  }
+
+  console.log('max layers hit')
+}
+function layersUpdate () {
+  const layerWrapper = document.querySelector("#tl-layers")
+
+  for (let i = $.LAYERS.length - 1; i >= 0 ; i--) {
+    if ($.LAYERS[i].name !== undefined && $.LAYERS[i].hidden !== undefined && $.LAYERS[i].locked !== undefined) {
+      layerWrapper.children[i].style.display = 'flex'
+
+      if ($.layerActive === i) {
+        layerWrapper.children[i].querySelector(`[data-name="name"]`).classList.add('active')
+      } else {
+        layerWrapper.children[i].querySelector(`[data-name="name"]`).classList.remove('active')
+      }
+
+      layerWrapper.children[i].querySelector(`[data-name="name"]`).innerText = $.LAYERS[i].name
+      layerWrapper.children[i].querySelector(`[data-name="lock"]`).src = $.LAYERS[i].locked ? 'img/lock.svg' : 'img/unlock.svg'
+      layerWrapper.children[i].querySelector(`[data-name="hide"]`).src = $.LAYERS[i].hidden ? 'img/eye.svg' : 'img/eye-active.svg'
+    } else {
+      layerWrapper.children[i].style.display = 'none'
+      layerWrapper.children[i].querySelector(`[data-name="name"]`).classList.remove('active')
+    }
+  }
+}
+function layersHide (e) {
+  const index = parseInt(e.target.dataset.i)
+  $.LAYERS[index].hidden = !$.LAYERS[index].hidden
+
+  const hidden = $.LAYERS[index].hidden
+
+  for (let i = 0; i < pixelResTotal; i++) {
+    const offset = i * 36
+
+    // Only toggle spots with color, otherwise we toggle empty indexes as well with no color
+    if (hidden && CURR_FRAME[offset + index] > 19) {
+      CURR_FRAME[offset + 32 + (~~(index / 8))] &= ~(128 >> (index & 7))
+    }
+
+    if (!hidden && CURR_FRAME[offset + index] > 19) {
+      CURR_FRAME[offset + 32 + (~~(index / 8))] |= (128 >> (index & 7))
+    }
+  }
+
+  layersUpdate()
+}
+function layersLock (e) {
+  const index = parseInt(e.target.dataset.i)
+  $.LAYERS[index].locked = !$.LAYERS[index].locked
+
+  layersUpdate()
+}
+function layersSwap (i1, i2) {
+  // swap temp refs in layers arr
+  const temp = $.LAYERS[i1]
+  $.LAYERS[i1] = $.LAYERS[i2]
+  $.LAYERS[i2] = temp
+
+  for (let pixI = 0, idx = 0; pixI < pixelResTotal; pixI++, idx+=36) { // for every pixel
+    // swap the respective i1, i2 layer index colors
+    const temp = CURR_FRAME[idx + i1]
+    CURR_FRAME[idx + i1] = CURR_FRAME[idx + i2]
+    CURR_FRAME[idx + i2] = temp
+
+    // for every color byte, if there is a number greater than 0 at this index, set respective bit to 1, otherwise set to 0
+    for (var i = 0; i < 32; i++) {
+      if (CURR_FRAME[idx + i] === 0) {
+        CURR_FRAME[idx + 32 + (~~(i / 8))] &= ~(128 >> (i & 7)) // flip down
+      } else {
+        CURR_FRAME[idx + 32 + (~~(i / 8))] |= (128 >> (i & 7)) // flip up
+      }
+    }
+  }
+
+  layersUpdate()
+}
+function layersInsertFromTo(from, to) {
+  if ($.LAYERS[from].name === undefined && $.LAYERS[from].hidden === undefined && $.LAYERS[from].locked === undefined) {
+    return
+  }
+
+  if ($.LAYERS[to].name === undefined && $.LAYERS[to].hidden === undefined && $.LAYERS[to].locked === undefined) {
+    return
+  }
+
+  if (from === to) return
+
+  // if from is less than to, its going up so
+  if (from < to) {
+    // swap with every next plus index after from until we've completed to
+    for (let i = from; i < to; i++) {
+      layersSwap(i, i + 1)
+    }
+  }
+
+  // if from is greater than to, we're going down
+  if (from > to) {
+    // swap with every next index minus 1 until to is reached
+    for (let i = from; i > to; i--) {
+      layersSwap(i, i - 1)
+    }
+  }
+
+  layersUpdate()
+}
+function layersRemove (e, index) {
+  const i = e ? $.layerActive : index
+
+  if ($.LAYERS[i].name === undefined && $.LAYERS[i].hidden === undefined && $.LAYERS[i].locked === undefined) {
+    return
+  }
+
+  let last
+  // If there there is layer above, so index minus 1
+  if ($.LAYERS[i - 1].name !== undefined || $.LAYERS[i - 1].hidden !== undefined || $.LAYERS[i - 1].locked !== undefined) {
+    // End is first layer index minus 1 that has all undefined
+    for (var a = 31; a >= 0; a--) {
+      if ($.LAYERS[a].name === undefined && $.LAYERS[a].hidden === undefined && $.LAYERS[a].locked === undefined) {
+        last = a + 1
+        break
+      }
+    }
+
+    layersInsertFromTo(i, last) // push this layer to the end
+  } else {
+    // Otherwise just delete curr index
+    last = i
+    $.layerActive = i === 31 ? 31 : i + 1 // it only one element, keep layeractive current, otherwise ,move it up one
+  }
+
+  // Then, delete it
+  $.LAYERS[last].name = last === 31 ? 'Layer 1' : undefined
+  $.LAYERS[last].hidden = last === 31 ? false : undefined
+  $.LAYERS[last].locked = last === 31 ? false : undefined
+
+  // For every pixel, set this particular layer index to 0 and flip the flag down
+  for (let pixI = 0, idx = 0; pixI < pixelResTotal; pixI++, idx+=36) {
+    CURR_FRAME[idx + last] = 0
+    CURR_FRAME[idx + 32 + (~~(last / 8))] &= ~(128 >> (last & 7))
+  }
+
+  layersUpdate()
+}
+function layersRename (e) {
+  // TODO: fix this query selector, lazy
+  const newName = document.querySelector('#rename-new-name').value
+
+  $.LAYERS[$.layerActive].name = newName
+
+  modalClose(undefined, 'rename')
+  layersUpdate()
+}
+
+window.addEventListener('mousedown', handleWindowEvents, false)
+window.addEventListener('mousemove', handleWindowEvents, false)
+window.addEventListener('mouseup', handleWindowEvents, false)
+window.addEventListener('dblclick', handleWindowEvents, false)
+
+let UI = {
+  mouseDown: false,
+  start: -1,
+  end: -1
+}
+
+function handleWindowEvents (e) {
+  if (e.type === 'mousedown') {
+    UI.mouseDown = true
+  }
+
+  // If over layers
+  if (e.target.dataset.tllayers !== undefined && e.target.dataset.name) {
+    // If mouse down, init tracking indexes
+    if (e.type === 'mousedown') {
+      UI.start = parseInt(e.target.dataset.i)
+      UI.end = parseInt(e.target.dataset.i)
+    }
+
+    if (UI.mouseDown && e.type === 'mousemove') {
+      // Get layer wrapper and assign it to target end drop
+      const target = e.target.closest('[data-layerindex]')
+
+      UI.end = parseInt(target.dataset.layerindex)
+
+      // Reset all layer button styles
+      let i = 0
+      while (i < target.parentNode.children.length) {
+        target.parentNode.children[i].style.boxShadow = 'none'
+        i++
+      }
+
+      // Set style of current target button
+      if (UI.start !== UI.end) {
+        target.style.boxShadow = `rgb(255, 255, 255) 0px ${UI.start > UI.end ? '2px' : '-2px'} 0px 0px inset`
+      }
+    }
+
+    if (e.type === 'dblclick') {
+      modalOpen(undefined, 'rename')
+    }
+  }
+
+  if (e.type === 'mouseup') {
+    // If indexes are not the same, make swap between start and end
+    // else if we're mouseup but still over layer, just set current index to active
+    if (UI.start !== UI.end) {
+      layersInsertFromTo(UI.start, UI.end)
+    } else if (e.target.dataset.tllayers !== undefined && e.target.dataset.name) {
+      layersSetActive(UI.start)
+    }
+
+    // Reset style of target button
+    if (UI.end !== -1 && UI.start !== UI.end) {
+      document.querySelector(`[data-layerindex="${UI.end}"]`).style.boxShadow = 'none'
+    }
+
+    UI.mouseDown = false
+    UI.start = -1
+    UI.end = -1
+
+    // TODO: clean flag below to be tied to window
+    if (!CANVAS.mouseDown) return
+
+    if ($.LAYERS[$.layerActive].locked || $.LAYERS[$.layerActive].hidden) {
+       return
+    }
+
+    if ($.toolActive === 'line' || $.toolActive === 'square' || $.toolActive === 'circle') {
+      if ($.toolActive === 'line') line('print', CANVAS.start, CANVAS.end, $.colorActive)
+      if ($.toolActive === 'square') square('print', CANVAS.start, CANVAS.end, $.colorActive)
+    }
+
+    if ($.toolActive === 'move' && CANVAS.isSelected) {
+      selectionWrite()
+    }
+
+    if ($.toolActive === 'select') {
+      selectionLift(CANVAS.start, CANVAS.end)
+      CANVAS.customSelection = true
+      setToolActive(undefined, 'move')
+    }
+
+    CANVAS.mouseDown = false
+  }
 }
 
 function main () {
   canvasInit(50, 50)
   canvasDraw()
+
+  layersInit()
 }
 
 main()

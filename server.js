e8bf98c43d29287d9d7c11a8141570ce18f071ee
JonFranco <jonfranco224@gmail.com>
2019-12-10 21:20:51 -0500

    fixing server

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const A = new ADOM({
   rootDir: './client' // tell adom where to look for adom files
 })
 
+const PORT = process.env.PORT || 8000
+
 require('http').createServer((req, res) => {
   res.writeHead(200, { 'Content-type': 'text/html' })
   console.log('building')
   res.end(A.render('index.adom'))
-}).listen(4000)
+}).listen(PORT)
 
 console.log('Running on 4000')
